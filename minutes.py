115752bcd532d38db8211a644bf6a6e7cc06c629
d3n2el <118548099+d3n2el@users.noreply.github.com>
2024-08-25 16:49:28 +0200

    Add files via upload

--- minutes.py
+++ minutes.py
@@ -1,42 +1,43 @@
-from datetime import date
-from datetime import timedelta
+from datetime import datetime, timedelta,date
 import re
 class Date:
-    date = datetime.date.today
-    def __init__(self, year, month, day):
-        if year == None or month == None or day == None:
-            raise ValueError("Invalid Time")
-        self.year= year
-        if int(month)> 12:
-            raise ValueError("Invalid month")
-        else:
-            self.month = month
-        if int(day)> 31:
-            raise ValueError("Invalid day")
-        else:
-            self.day = day
+    def __init__(self, time=None):
+        self.time = time if time else date.today()
 
     def __sub__(self, other):
-        time = datetime(year,month,day)
-        minutes= 
-        return time
+        timediff = self.time - other.time
+        return timediff.days * 24 * 60 + timediff.seconds // 60
+    def __str__(self):
+        return f"{self.time}"
 
 def main():
-    #print the converted time. Will probably make a convert function
-    #also, ask for input]
     print(convert(input("Date of birth: ")))
 
 def convert(t):
     # need to elaborate input and split it
-    year,month,day = re.search(r"([0-9]{4})-([0-9]{2})-([0-9]{2})", t)
+    time = re.search(r"([0-9]{4})-([0-9]{2})-([0-9]{2})", t)
+    year,month,day = time.groups()
+    #validate input through date module
+    try:
+        date_= date.today()
+        year= int(year)
+        month= int(month)
+        day = int(day)
+        d = date(year,month,day)
+        final= date_ - d
+        return final
 
-    #either .split or re.split
-    #Probably re.split since its way more flexible, since i want to get yyyy-mm-dd format
-    #will probably need to convert the time in int
-    # override - so that it returns a timedelta
+    except ValueError as e:
+        sys.exit(f"{e}: Invalid Date")
+
+
+    # If no exception is raised, the format is correct
+    # Handle the error if the format is incorrect
+    #if not valid,raise valueError
+    #else: pass the data to Data
+    #now i need to understand where i use inflect
     # will use inflect to return the written minutes properly
     #acutally return the value so that main can print it
-    #Also need to properly define Date, as of rn its barebones
 
 
 
