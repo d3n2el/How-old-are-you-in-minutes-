841beb5f35ccb7d15a763fee1054db748e5b26cf
d3n2el <118548099+d3n2el@users.noreply.github.com>
2024-08-25 13:38:31 +0200

    Add files via upload

---
+++ minutes.py
@@ -0,0 +1,50 @@
+from datetime import date
+from datetime import timedelta
+import re
+class Date:
+    date = datetime.date.today
+    def __init__(self, year, month, day):
+        if year == None or month == None or day == None:
+            raise ValueError("Invalid Time")
+        self.year= year
+        if int(month)> 12:
+            raise ValueError("Invalid month")
+        else:
+            self.month = month
+        if int(day)> 31:
+            raise ValueError("Invalid day")
+        else:
+            self.day = day
+
+    def __sub__(self, other):
+        date = datetime.date.today
+        year= timedelta(self.year,date)
+        month= timedelta(self.month,date)
+        day = timedelta(self.day,date)
+        time = int(year)+int(month)+int(day)
+        return time
+
+def main():
+    #print the converted time. Will probably make a convert function
+    #also, ask for input]
+    print(convert(input("Date of birth: ")))
+
+def convert(t):
+    # need to elaborate input and split it
+    year,month,day = re.search(r"([0-9]{4})-([0-9]{2})-([0-9]{2})", t)
+
+    #either .split or re.split
+    #Probably re.split since its way more flexible, since i want to get yyyy-mm-dd format
+    #will probably need to convert the time in int
+    # override - so that it returns a timedelta
+    # will use inflect to return the written minutes properly
+    #acutally return the value so that main can print it
+    #Also need to properly define Date, as of rn its barebones
+
+
+
+...
+
+
+if __name__ == "__main__":
+    main()
